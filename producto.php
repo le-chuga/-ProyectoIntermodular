c3c19eb1b0bb85da850bc274dbaa8be69089c484
le-chuga <Manelviel2@gmail.com>
2026-05-05 02:59:12 +0200

    errores corregidos

--- producto.php
+++ producto.php
@@ -16,17 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         die("Error producto: " . $mysqli->error);
     }
 
-    echo "<p>✅ Producto insertado correctamente</p>";
+    echo "<p> Producto insertado correctamente</p>";
 }
 ?>
 
-<h2>Gestionar Producto</h2>
-<form method="POST">
-    <input name="codigoproducto" placeholder="Código producto" required><br>
-    <input name="descripcionproducto" placeholder="Descripción" required><br>
-    <input name="codigoproveedorproducto" placeholder="Código proveedor" required><br>
-    <input name="preciocompraproducto" placeholder="Precio compra" type="number" step="0.01"><br>
-    <input name="precioventaproducto" placeholder="Precio venta" type="number" step="0.01"><br>
-    <input name="stockproducto" placeholder="Stock" type="number"><br>
-    <button type="submit">Guardar</button>
-</form>
